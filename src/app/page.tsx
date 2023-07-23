418b16a5616376e43d35238ab38ba4277f435bb1
manraj singh <vmanrajsingh@gmail.com>
2023-07-23 23:17:23 +0530

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,13 @@
 import Navbar from "@/components/Navbar";
 import Subject from "@/components/Subject";
 import useAlan from "@/helpers/chatbot";
-import { useRef } from "react";
 
 export default function Home() {
-  const alanBtnContainer = useRef(null);
   useAlan();
   return (
     <>
       <Navbar />
       <Subject />
-      <div ref={alanBtnContainer} />
     </>
   );
 }
