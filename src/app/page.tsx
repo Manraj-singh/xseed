5004fed2d472d296727702333d616b97dbc4e697
manraj singh <vmanrajsingh@gmail.com>
2023-07-23 23:21:04 +0530

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import Subject from "@/components/Subject";
-import useAlan from "@/helpers/chatbot";
 
 export default function Home() {
-  useAlan();
   return (
     <>
       <Navbar />
